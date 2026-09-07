1ca6232dbe53d009112838d088900f1adb5e5de7
Andres Gomez <nnandresparra@gmail.com>
2026-09-07 17:41:48 -0500

    Correccion error web

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from routes.horario_route import routes
 from config import Config
+from flask_cors import CORS
 
 app = Flask(__name__)
-
+CORS(app, resources={r"/*": {"origins": "*"}})
 app.config.from_object(Config)
 app.register_blueprint(routes)
 
